2a90bc251bda5bfbb5e1402606613ae725934cd7
fantasy1022 <fantasy-1022@hotmail.com>
2020-08-31 14:21:01 +0800

    Implement the storage runtime permission.

--- app/src/main/java/com/fantasyfang/fancy/ui/song/SongListFragment.kt
+++ app/src/main/java/com/fantasyfang/fancy/ui/song/SongListFragment.kt
@@ -1,12 +1,18 @@
 package com.fantasyfang.fancy.ui.song
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.fantasyfang.fancy.R
+
+private const val READ_EXTERNAL_STORAGE_REQUEST = 1
 
 class SongListFragment : Fragment() {
 
@@ -14,13 +20,60 @@ class SongListFragment : Fragment() {
         fun newInstance() = SongListFragment()
     }
 
-    private  val songListViewModel: SongListViewModel by viewModels()
+    private val songListViewModel: SongListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.song_list_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_song_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        openMediaStore()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            READ_EXTERNAL_STORAGE_REQUEST -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showSongs()
+                }
+            }
+        }
+    }
+
+    private fun haveStoragePermission() =
+        ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestPermission() {
+        if (!haveStoragePermission()) {
+            val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                permissions,
+                READ_EXTERNAL_STORAGE_REQUEST
+            )
+        }
+    }
+
+    private fun showSongs() {
+        //TODO: show song list
+    }
+
+    private fun openMediaStore() {
+        if (haveStoragePermission()) {
+            showSongs()
+        } else {
+            requestPermission()
+        }
+    }
 }
