06dfde1c58bcb0cfb3f9ab4f5a43898f76fb7795
fantasy1022 <fantasy-1022@hotmail.com>
2020-09-05 17:23:52 +0800

    Implement music service

---
+++ app/src/main/java/com/fantasyfang/fancy/media/MusicService.kt
@@ -0,0 +1,39 @@
+package com.fantasyfang.fancy.media
+
+import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import androidx.media.MediaBrowserServiceCompat
+import com.fantasyfang.fancy.BuildConfig
+
+class MusicService : MediaBrowserServiceCompat() {
+
+    override fun onLoadChildren(
+        parentId: String,
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+    ) {
+        //Do nothing
+    }
+
+    override fun onGetRoot(
+        clientPackageName: String,
+        clientUid: Int,
+        rootHints: Bundle?
+    ): BrowserRoot? {
+        return if (allowBrowsing(clientPackageName)) {
+            BrowserRoot(FANCY_BROWSABLE_ROOT, null)
+        } else {
+            if (BuildConfig.DEBUG) {
+                BrowserRoot(FANCY_EMPTY_ROOT, null)
+            } else {
+                null
+            }
+        }
+    }
+
+    private fun allowBrowsing(clientPackageName: String): Boolean {
+        return clientPackageName == packageName
+    }
+}
+
+const val FANCY_BROWSABLE_ROOT = "/"
+const val FANCY_EMPTY_ROOT = "@empty@"
